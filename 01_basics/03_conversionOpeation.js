56c19ae4b085ba7779a6cc221ab649600014c467
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-06-26 05:20:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpeation.js
@@ -0,0 +1,25 @@
+let score="836kd"
+// console.log(typeof score)
+
+// let valueNumber=Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+/*"33"=>isko jab console.log(valueNumber) to print ho ga actual number 
+but wahi jab "33jgd" kuch alphabet then NaN(nt a nmbr)
+true->1;
+false->0;
+*/
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(typeof isloggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+/*
+1->True;
+0->false;
+"Kamran"->true
+*/
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof someNumber)
+console.log(typeof stringNumber)
