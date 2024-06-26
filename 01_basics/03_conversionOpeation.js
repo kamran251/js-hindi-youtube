bbd60374a622184d26747ea1c4e7b9c832471501
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-06-26 11:05:39 +0000

    conversion datatypes

--- 01_basics/03_conversionOpeation.js
+++ 01_basics/03_conversionOpeation.js
@@ -1,4 +1,4 @@
-let score="836kd"
+// let score="836kd"
 // console.log(typeof score)
 
 // let valueNumber=Number(score)
@@ -9,17 +9,33 @@ but wahi jab "33jgd" kuch alphabet then NaN(nt a nmbr)
 true->1;
 false->0;
 */
-let isloggedIn=1
-let booleanIsLoggedIn=Boolean(isloggedIn)
-console.log(typeof isloggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isloggedIn=1
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+// console.log(typeof isloggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 /*
 1->True;
 0->false;
 "Kamran"->true
 */
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(typeof someNumber)
-console.log(typeof stringNumber)
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(typeof someNumber)
+// console.log(typeof stringNumber)
+
+//      *** OPERATIONS ***             //
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(21%10)
+// let string1="Kamran"
+// let string2=" Hwllo"
+// console.log(string1+ string2)
+// let string3=string1+string2
+// console.log(string3)
+console.log("2"+1)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
