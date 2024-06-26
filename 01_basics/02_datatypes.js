474a3233c582d022a5d64af2dd8309b65bcd148f
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-06-26 04:42:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as a newer version
+//alert(3+3) // we are using nodejs not browser
+console.log(3
+    +
+    3) ;//Dont do this type bcaz of the readability problem
+console.log("hello");
+let name="kamran"
+let age=22
+let isloggedIn=false
+
+/*number=>2 to power of 55
+bigint=>agar number bahut bada hai
+string=' ' or " "
+boolean=>true/false
+null=>standlone value(ye ek representation hai empty value ka )
+undefined=>means variable define kr diya hai but value assign nahi kiya 
+symbol=>uniqeness find krne ke liye use of symbol
+object
+*/
+console.log(typeof "kamran")
+console.log(typeof 22)
+console.log(typeof null)//ye null type object(interview question)
+console.log(typeof undefined)//undefined apne me hi ek type hai
