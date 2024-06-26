46c0d49d02cf20a6cea13e22eeae43eb691a72e1
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-06-26 12:29:21 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// premitive 
+
+//7 types: string,number,boolean,null,udefined,symbol,bigInt
+const score=68;
+const scorevalue=68.3;
+
+const isloggedIn=false;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)//false
+const bigInt=8696n
+
+
+
+
+
+
+
+
+//reference (Non primitive)
+//Array,object,functions
+const heros=["shaktiman","nagraaj","doga"];
+let myobj={
+    name:"kamran",
+    age:22,
+}//means curly bracket ke andar jo bhi aaiga wo object datatypes kuch bhi ho skta hai
+/*
+                          TYPE OF OPERATORS RESULTS
+              type of val            -->         result
+              undefined                         " undefined "
+              null                              " object "
+              boolean                           " boolean "
+              number                            " number "
+              string                             " string "
+              symbol                             " symbol "
+              Function                           " object function "
+
+*/
