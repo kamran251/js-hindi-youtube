81054b20fc1690c18cd6b966241da3b6d51a474f
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-06-27 11:42:59 +0000

    num and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// const otherNumber=12.3390
+// console.log(otherNumber.toPrecision(3))//mns 3 number ka dekh kr round off value do
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+// +++++++++++++++++      Maths      ++++++++++++++++++++++++++++++++++
+console.log(Math.abs(-9))//-ve ko +ve me badal deta hai 
+console.log(Math.round(6.895));
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.9))
+console.log(Math.min(2,7,3,21,89))
+console.log(Math.max(2,6,22,78))
+
+console.log(Math.random())//ye 0 se 1 ke bich ka number deti hai
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
