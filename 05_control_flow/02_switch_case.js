6fde39e519b3e0694d030c44dc10ce1d140806d1
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-07-02 08:48:37 +0000

    switch case

---
+++ 05_control_flow/02_switch_case.js
@@ -0,0 +1,19 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+//}
+const month="march"
+switch(month){
+    case "jan":console.log("jan");
+    break;
+    case "feb":console.log("feb");
+    break;
+    case "march":console.log("mar");
+    break;
+    default:console.log("apr");
+    break;
+}
