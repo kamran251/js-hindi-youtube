f62fac5d7a13c29f1bc1033720db723e01d38038
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-07-02 19:09:24 +0000

    while loops

---
+++ 05_iterations/02_while_do_while.js
@@ -0,0 +1,11 @@
+// while 
+// let index=0
+// while(index <=10){
+//     console.log(`value of index is ${index}`)
+//     index=index+3;
+// }
+let score=11
+do{
+    console.log(`score is ${score}`)
+    score++
+} while(score<=10)
