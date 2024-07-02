97431f1603c71d29c753e13b42e1c51b8d13703c
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-07-02 19:09:58 +0000

    for in loops

---
+++ 05_iterations/04_for_of2.js
@@ -0,0 +1,15 @@
+const myObject={
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+
+}
+
+//forin used
+// for (const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`)
+//}
+// for (const key in myObject){
+//     console.log(myObject[key])
+// }
