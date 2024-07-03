47c420ce87f0a5f56cdb03eb56d1f3ae4c9d4644
kamran251 <82179802+kamran251@users.noreply.github.com>
2024-07-03 17:46:50 +0000

    forof part 2

--- 05_iterations/04_for_of2.js
+++ 05_iterations/04_for_of2.js
@@ -13,3 +13,13 @@ const myObject={
 // for (const key in myObject){
 //     console.log(myObject[key])
 // }
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"united states of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+// for (const key in map){
+//     console.log(key);
+// }// cant apply
+
